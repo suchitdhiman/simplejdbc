ec09acfaeefbfe6acc9b330e53951a82577a8e55
Suchit kumar <dhiman.suchit@gmail.com>
2024-12-12 22:08:35 +0530

    full array project

---
+++ ArrayProject/src/com/array/ArraySecond.java
@@ -0,0 +1,25 @@
+package com.array;
+
+public class ArraySecond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[] = {12, 13, 14, 15, 16};
+		
+		int firstValue = arr[0];
+		int lastValue = arr[arr.length-1];
+		int temp = lastValue;
+		
+		lastValue = firstValue;
+		firstValue = temp;
+		
+		arr[0] = temp;
+		arr[arr.length-1] = lastValue;
+		for(int i = 0; i< arr.length; i++) {
+			System.out.println(arr[i]);
+		}	
+
+	}
+
+}
