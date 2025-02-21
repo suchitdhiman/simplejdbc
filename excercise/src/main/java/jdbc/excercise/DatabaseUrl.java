d64e1ffbb3d57113f502d9464c188364331cbe13
Suchit kumar <dhiman.suchit@gmail.com>
2025-02-21 15:14:26 +0530

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: Ia4c83eef28f865902a790e3e089eb824189b226c

---
+++ excercise/src/main/java/jdbc/excercise/DatabaseUrl.java
@@ -0,0 +1,8 @@
+package jdbc.excercise;
+
+public class DatabaseUrl {
+	public static final String url = "jdbc:mysql://localhost:3306/world";
+	public static final String username = "root";
+	public static final String password = "sk8006424";
+
+}
