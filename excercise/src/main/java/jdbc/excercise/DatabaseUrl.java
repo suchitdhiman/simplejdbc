9003d5905536a744ddca20383dd3f160fc4115ff
Suchit Kumar <85825609+suchitdhiman@users.noreply.github.com>
2025-03-09 19:00:02 +0530

    Delete excercise directory

--- excercise/src/main/java/jdbc/excercise/DatabaseUrl.java
+++
@@ -1,8 +0,0 @@
-package jdbc.excercise;
-
-public class DatabaseUrl {
-	public static final String url = "jdbc:mysql://localhost:3306/world";
-	public static final String username = "root";
-	public static final String password = "sk8006424";
-
-}
